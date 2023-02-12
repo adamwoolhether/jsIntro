8c1be6893c6e686e5a7c82ac074f402595bda628
adamwoolhether <adamwoolhether@gmail.com>
2023-02-12 23:37:14 +0800

    refactor factorial demo to use array

--- fact/fact.js
+++ fact/fact.js
@@ -1,23 +1,36 @@
 function factTable(formId) {
+    // Get limit value.
     let limit = formId.textin.value;
 
+    // Validate the input number.
+    let result;
     if (limit >= 1) {
         let val = 1;
         let fact = 1;
-        var result = "<pre>VALUE FACTORIAL\n";
+        let index = 0;
+        let factTb = [];
 
-        for(let count = 1; count <= limit; count++) {
-            result += val + "    " + fact + "\n";
+        // Compute values, storing the in an array.
+        for (let count = 1; count <= limit; count++) {
+            factTb[index++] = fact;
             fact = fact * ++val;
             if (fact > 999999999999999) {
-                result += "value to large";
                 break;
             }
+        }
+
+        // Build the output string.
+        let result = "<pre>VALUE FACTORIAL\n";
+        for (let i = 0; i < factTb.length; i++) {
+            val = i + 1;
+            fact = factTb[i];
+            result += val + "      " + fact + "\n";
         }
         result += "</pre>";
     } else {
         result = "Invalid limit!";
     }
 
+    // output the table to UI.
     document.getElementById("textout").innerHTML = result;
 }
