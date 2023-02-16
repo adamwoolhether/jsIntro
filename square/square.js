6fb8b18e17db4bce9bdc2cb12c70bf29f93f8749
adamwoolhether <adamwoolhether@gmail.com>
2023-02-16 21:48:03 +0800

    err example

--- square/square.js
+++ square/square.js
@@ -1,12 +1,22 @@
-function findSquare(formId) {
-    const inputNum = formId.textin.value;
+function findSquare(formid) {
+    // Try to do the following, but watch for errors
+    try{
+        const inputNum = formId.textin.value;
 
-    let result;
-    if (inputNum >= 1 && inputNum <= 10) {
-        result = inputNum*inputNum;
-    } else {
-        result = "Invalid number!"
-    }
+        let result;
+        if (inputNum >= 1 && inputNum <= 10) {
+            result = inputNum*inputNum;
+        } else {
+            result = "Invalid number!"
+        }
+        // Display a message if an error was detected.
+    } catch(err) {
+        result = err.message;
+    }/* } finally {
+    * document.getElementById("textout").innerHTML = result;
+    * }
+    * */
+
 
     document.getElementById("textout").innerHTML = result;
 }
